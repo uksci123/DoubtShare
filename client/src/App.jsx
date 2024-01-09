133db3f45fe6a5ae968cb51cf05c6b3c84ece313
Utsav Yadav <utsavyadav@Utsavs-MacBook-Air.local>
2024-01-09 11:37:19 +0530

    first commit initialising frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+const App = ()=> {
+  return (
+    <>
+      Started Assissment of revly.io
+    </>
+  )
+}
+
+export default App
